5fdbca7f3a363ff944318a49f903ebbe6c8d4683
vnzz477 <ra54865444807@acaonsfatima.org.br>
2025-09-18 15:06:40 -0300

    Reverte para React 18 para compatibilidade de build no Netlify

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/inicio/Inicio";
 import Cadastro from "./pages/cadastro/Cadastro";
 import LoginAluno from "./pages/login/Login";
